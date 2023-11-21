f6ecaadb0e8dc864486ec4bb2c2880c8f23f3cd0
LutfiIsnanSafrudin <i.snans.afrudin@gmail.com>
2023-11-21 21:55:51 +0700

    Tugas Chapter
    untuk perbaikan menyusul

---
+++ bioskop-backend-Chapter-4/src/main/java/org/binaracademy/bioskopbackend/BioskopbackendApplication.java
@@ -0,0 +1,18 @@
+package org.binaracademy.bioskopbackend;
+
+import org.binaracademy.bioskopbackend.controller.MovieController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.ParseException;
+
+@SpringBootApplication
+public class BioskopbackendApplication {
+
+	public static void main(String[] args) throws ParseException {
+		SpringApplication.run(BioskopbackendApplication.class, args);
+		System.out.println("Thank you for using this app!");
+	}
+
+}
